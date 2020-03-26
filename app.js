2d79e91eaf1feea164fcf1ce761249a38378ecef
John Doe <nadia643@gmail.com>
2020-03-26 10:34:04 +0000

    console logs the current number updating

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ function numberToScreen(value) {
     current = document.getElementById("input").value;
     current = current + value;
     document.getElementById("input").value = current;
+    console.log(current);
 }
 
 //clear calculator screen
@@ -16,7 +17,8 @@ window.onload =  clearScreen();
 //delete function
 function deleteNumber(value) {
     value = document.getElementById("input").value;
-    document.getElementById("input").value = current.substr(0, value.length - 1);    
+    document.getElementById("input").value = current.substr(0, value.length - 1);   
+    console.log(value); 
 }
 
 //calculate function
@@ -24,6 +26,11 @@ function calculate() {
     const values = document.getElementsByClassName("number.").parseInt(value);
     const ans = values + values;
     console.log(ans);
+}
+
+//add function
+function add(x, y) {
+    console.log(x + y)
 }
 
     
